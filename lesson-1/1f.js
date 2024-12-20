59acc6edc0135f731d4bac1aa4c1e3a08494dc9b
Vansh Patel <patel.vansh1607@gmail.com>
2024-12-20 22:07:42 +0300

    answered correctly

---
+++ lesson-1/1f.js
@@ -0,0 +1,9 @@
+//You order a T-shirt for $10, socks for $8, and dinner plates for $20.
+// Use JavaScript to calculate the total cost of your order.
+
+let tShirt = 10;
+let socks = 8;
+let dinnerPlates = 20;
+
+cost = tShirt+ socks + dinnerPlates;
+console.log(cost);  
